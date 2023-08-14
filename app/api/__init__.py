950e74d0cb7985bfd64ebb8ab3286f0ac9202ba2
Borja Esteban <boressan@outlook.com>
2023-08-14 12:13:28 +0200

    implement api versioning by multi app

---
+++ app/api/__init__.py
@@ -0,0 +1,4 @@
+from app.api import v1
+from app.api import example
+
+__all__ = ["v1", "example"]
--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
+"""Description to be changed later at:
+    - backend/app/main.py
+"""
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from starlette.middleware.cors import CORSMiddleware
 
-from app.api.api_v1.api import api_router
+from app import api
 from app.core.config import settings
-from app.gdb import NeomodelConfig
 
+# Create FastAPI instance
 app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    title=settings.PROJECT_NAME,
+    description=__doc__,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
 
 # Set all CORS enabled origins
@@ -19,7 +25,20 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
-app.include_router(api_router, prefix=settings.API_V1_STR)
 
-#nmc = NeomodelConfig()
-#nmc.ready()
+# Set "latest" api as mounted app
+api_latests = FastAPI()
+app.mount("/latest", api_latests)
+api_latests.include_router(api.example.api_router)
+
+
+# Set "v1" api as mounted app
+api_v1 = FastAPI()
+app.mount("/v1", api_v1)
+api_v1.include_router(api.v1.api_router)
+
+
+# Set "example" api as mounted app
+api_example = FastAPI()
+app.mount("/example", api_example)
+api_example.include_router(api.example.api_router)
