1e3afb0ae76849fd90a2a2bf4c5131ccc8b8a97d
Borja Esteban <boressan@outlook.com>
2023-08-15 10:28:38 +0200

    mv main.py to __main__.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 
 from app.core.config import settings
 from app.db.session import SessionLocal
-from app.main import app
+from app.__main__ import app
 from app.tests.utils.user import authentication_token_from_email
 from app.tests.utils.utils import get_superuser_token_headers
 
