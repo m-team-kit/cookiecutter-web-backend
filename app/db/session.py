b4899ee0004dc51b2304cf3397345baf429c2af3
Borja Esteban <boressan@outlook.com>
2023-08-15 11:17:45 +0200

    fix session url

--- app/db/session.py
+++ app/db/session.py
@@ -3,5 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+# Disconnect Handling - Pessimistic
+engine = create_engine(f"{settings.SQLALCHEMY_DATABASE_URI}", pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
