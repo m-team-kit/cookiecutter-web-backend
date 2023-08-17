b303dd5395463638fb7be75845b3e0df444bf4ea
Borja Esteban <boressan@outlook.com>
2023-08-17 11:02:39 +0200

    add api to urls

--- app/__main__.py
+++ app/__main__.py
@@ -12,7 +12,7 @@ from app.core.config import settings
 app = FastAPI(
     title=settings.PROJECT_NAME,
     description=__doc__,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    openapi_url="/api/openapi.json",
 )
 
 # Set all CORS enabled origins
@@ -35,11 +35,11 @@ def favicon():
 
 # Set "latest" api as mounted app
 api_latests = FastAPI()
-app.mount("/latest", api_latests)
+app.mount("/api/latest", api_latests)
 api_latests.include_router(api.v1.api_router)
 
 
 # Set "v1" api as mounted app
 api_v1 = FastAPI()
-app.mount("/v1", api_v1)
+app.mount("/api/v1", api_v1)
 api_v1.include_router(api.v1.api_router)
