822986e7b91e12f7622ce02455d4eaeb34a893d2
Borja Esteban <boressan@outlook.com>
2023-08-18 11:19:45 +0200

    fix auth get_user

--- app/core/authentication.py
+++ app/core/authentication.py
@@ -1,10 +1,12 @@
 from fastapi import Depends
-from fastapi.security import HTTPBearer
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from flaat.fastapi import Flaat
 from sqlalchemy.orm import Session
 
 from app import db
+from app import crud
 from app.core.config import settings
+from app import models
 
 bearer_token = HTTPBearer()
 flaat = Flaat()
@@ -13,9 +15,8 @@ flaat.set_trusted_OP_list([str(x) for x in settings.TRUSTED_OP_LIST])
 
 async def get_user(
     session: Session = Depends(db.get_session),
-    token: str = Depends(bearer_token),
-) -> str:  # models.User:
+    token: HTTPAuthorizationCredentials = Depends(bearer_token),
+) -> models.User:
     token_info = flaat.get_user_infos_from_access_token(token.credentials)
-    subiss = (token_info.subject, token_info.issuer)
-    # db_user = crud.user.get(db=session, id=subiss)
-    return subiss
+    subiss = token_info.subject, token_info.issuer
+    return crud.user.get(session, id=subiss)
