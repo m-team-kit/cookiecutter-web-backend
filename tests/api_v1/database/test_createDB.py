4ad95b5f493d9089308183e6ef41d5f5e7dd88cc
Borja Esteban <boressan@outlook.com>
2023-08-22 08:58:25 +0200

    add DB templates and scores tests to createDB tests

--- tests/api_v1/database/test_createDB.py
+++ tests/api_v1/database/test_createDB.py
@@ -1,8 +1,10 @@
 # pylint: disable=redefined-outer-name
-from typing import Dict
+from uuid import UUID
+from typing import Dict, List
 import pytest
 from fastapi import Response
 from fastapi.testclient import TestClient
+from app.models.template import Template
 
 
 @pytest.fixture(scope="module")
@@ -35,3 +37,27 @@ def test_HTTP_403_UNAUTHORIZED(response: Response) -> None:
     """Tests the response status code is 403 and valid."""
     assert response.status_code == 403
     assert response.json() == {"detail": "Not authenticated"}
+
+
+@pytest.mark.parametrize("custom", [{"secret": "6de44315b565ea73f778282d"}], indirect=True)
+@pytest.mark.parametrize("headers", [{"authorization": "bearer 6de44315b565ea73f778282d"}], indirect=True)
+def test_DB_templates(response: Response, templates: List[Template]) -> None:
+    """Tests the database contains the correct templates."""
+    assert isinstance(templates[0].id, UUID)
+    assert templates[0].repoFile == "my_template_1.json"
+    assert templates[0].title == "My Template 1"
+    assert templates[0].summary == "Template example 1"
+    assert templates[0].language == "Python"
+    assert templates[0].tags == ["Tag1", "Tag2"]
+    assert templates[0].picture == "https://picture-url/template_1"
+    assert templates[0].gitLink == "https://some-git-link/template_1"
+    assert templates[0].gitCheckout == "main"
+
+
+@pytest.mark.parametrize("custom", [{"secret": "6de44315b565ea73f778282d"}], indirect=True)
+@pytest.mark.parametrize("headers", [{"authorization": "bearer 6de44315b565ea73f778282d"}], indirect=True)
+def test_DB_scores(response: Response, templates: List[Template]) -> None:
+    """Tests the database contains the correct templates."""
+    for template in templates:
+        assert template.score == 0.0
+        assert template.scores == ""
