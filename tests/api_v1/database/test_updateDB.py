c9aab462e1b959039b7ded6c85017a926ef02b8d
Borja Esteban <boressan@outlook.com>
2023-08-24 09:09:34 +0200

    more compact tests

--- tests/api_v1/database/test_updateDB.py
+++ tests/api_v1/database/test_updateDB.py
@@ -17,37 +17,15 @@ def response(client: TestClient, headers: Dict) -> None:
 
 
 @pytest.mark.parametrize("headers", [{"authorization": "bearer 6de44315b565ea73f778282d"}], indirect=True)
-def test_204_no_content(response: Response) -> None:
+def test_204_no_content(response: Response, templates: List[Template]) -> None:
     """Tests the response status code is 204 and valid."""
+    # Assert response is valid
     assert response.status_code == 204
-
-
-@pytest.mark.parametrize("headers", [{"authorization": "bearer bad-secret"}], indirect=True)
-def test_401_unauthorized(response: Response) -> None:
-    """Tests the response status code is 401 and valid."""
-    assert response.status_code == 401
-    assert response.json() == {"detail": "Incorrect secret"}
-    assert response.headers["WWW-Authenticate"] == "Bearer"
-
-
-@pytest.mark.parametrize("headers", [{}], indirect=True)
-def test_403_forbidden(response: Response) -> None:
-    """Tests the response status code is 403 and valid."""
-    assert response.status_code == 403
-    assert response.json() == {"detail": "Not authenticated"}
-
-
-@pytest.mark.parametrize("headers", [{"authorization": "bearer 6de44315b565ea73f778282d"}], indirect=True)
-def test_db_length(templates: List[Template]) -> None:
-    """Tests the database contains the correct templates."""
+    # Assert database contains the correct templates
     assert len(templates) == 4
     assert "my_template_3" not in templates
     assert "my_template_5" in templates
-
-
-@pytest.mark.parametrize("headers", [{"authorization": "bearer 6de44315b565ea73f778282d"}], indirect=True)
-def test_db_templates(templates: List[Template]) -> None:
-    """Tests the database contains the correct templates."""
+    # Assert templates are correct
     assert isinstance(templates["my_template_1"].id, UUID)
     assert templates["my_template_1"].repoFile == "my_template_1.json"
     assert templates["my_template_1"].title == "Edited Template 1"
@@ -57,12 +35,30 @@ def test_db_templates(templates: List[Template]) -> None:
     assert templates["my_template_1"].picture == "https://picture-url/template_1"
     assert templates["my_template_1"].gitLink == "https://some-git-link/template_1"
     assert templates["my_template_1"].gitCheckout == "dev"
-
-
-@pytest.mark.parametrize("headers", [{"authorization": "bearer 6de44315b565ea73f778282d"}], indirect=True)
-def test_db_scores(templates: List[Template]) -> None:
-    """Tests the database contains the correct templates."""
+    # Assert scores are correct
     assert templates["my_template_1"].score is not None
     assert templates["my_template_2"].score is not None
     assert templates["my_template_4"].score is None
     assert templates["my_template_5"].score is None
+
+
+@pytest.mark.parametrize("headers", [{"authorization": "bearer bad-secret"}], indirect=True)
+def test_401_unauthorized(response: Response) -> None:
+    """Tests the response status code is 401 and valid."""
+    # Assert response is valid
+    assert response.status_code == 401
+    # Assert header is valid
+    assert response.headers["WWW-Authenticate"] == "Bearer"
+    # Assert message is valid
+    message = response.json()
+    assert message == {"detail": "Incorrect secret"}
+
+
+@pytest.mark.parametrize("headers", [{}], indirect=True)
+def test_403_forbidden(response: Response) -> None:
+    """Tests the response status code is 403 and valid."""
+    # Asset response is valid
+    assert response.status_code == 403
+    # Assert message is valid
+    message = response.json()
+    assert message == {"detail": "Not authenticated"}
