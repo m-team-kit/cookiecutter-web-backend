f589d6590a41615ace1a9669730b2b7502902a98
Borja Esteban <boressan@outlook.com>
2023-10-31 17:40:11 +0100

    implement auto database upgrade

--- autoapp.py
+++ autoapp.py
@@ -1,5 +1,13 @@
 """Create an application instance."""
+import logging
+
 from fastapi import FastAPI
+
+from alembic import command
+from alembic.config import Config
 from app import create_app
 
 app: FastAPI = create_app()
+logging.info("Running DB migrations")
+alembic_cfg = Config("alembic.ini")
+command.upgrade(alembic_cfg, "head")
